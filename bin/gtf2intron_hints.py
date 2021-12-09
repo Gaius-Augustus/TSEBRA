000f723434f1273217d7ce464f29cfdc145fef9b
Lars Gabriel <lgabriel23@gmx.de>
2021-12-09 15:43:25 +0100

    fixed gtf2intron_hints

--- bin/gtf2intron_hints.py
+++ bin/gtf2intron_hints.py
@@ -19,16 +19,14 @@ def main():
     from overlap_graph import Graph
     from evidence import Evidence
 
-    global
-
     args = parseCmd()
     src = 'M'
-    if args.s and len(args.s) > 1:
+    if args.src and len(args.src) > 1:
         sys.stderr.write('### ERROR: Please provide only a single character ' \
             + 'as hint source with --src!')
         exit()
-    if args.s:
-        src = args.s
+    if args.src:
+        src = args.src
     # read gene prediciton file
     sys.stderr.write('### READING GENE SET: [{}]\n'.format(args.gtf))
     gtf = Anno(args.gtf, '')
@@ -38,7 +36,7 @@ def main():
     intron_keys = {}
 
     for tx in gtf.transcripts.values():
-        introns = tx.get_type_coords(intron)
+        introns = tx.get_type_coords('intron')
         for i in introns.values():
             for intron in i:
                 key = "{};{};{};{}".format(tx.chr, tx.strand, intron[0], intron[1])
@@ -51,7 +49,7 @@ def main():
         output.append([key[0], 'tsebra_hints', 'intron', int(key[2]), int(key[3]), \
             intron_keys[intron], key[1], '.', 'mult={};pri=4;src={}'.\
             format(intron_keys[intron], src)])
-    output.sort(key = lambda i:(i[3], i[4]))
+    output.sort(key = lambda i:(i[0], i[3], i[4], i[6]))
 
     with open(args.out, 'w+') as file:
         out_writer = csv.writer(file, delimiter='\t', quotechar = "|", lineterminator = '\n')
