d60cc681d197e432785f64cf12619b4bf0119161
Lars Gabriel <>
2021-12-09 14:43:27 +0100

    added script gtf2intron_hints.py

---
+++ bin/gtf2intron_hints.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# ==============================================================
+# author: Lars Gabriel
+#
+# TSEBRA: Transcript Selector for BRAKER
+# ==============================================================
+import argparse
+import sys
+import os
+import csv
+
+class ConfigFileError(Exception):
+    pass
+
+gtf = None
+
+def main():
+    from genome_anno import Anno
+    from overlap_graph import Graph
+    from evidence import Evidence
+
+    global
+
+    args = parseCmd()
+    src = 'M'
+    if args.s and len(args.s) > 1:
+        sys.stderr.write('### ERROR: Please provide only a single character ' \
+            + 'as hint source with --src!')
+        exit()
+    if args.s:
+        src = args.s
+    # read gene prediciton file
+    sys.stderr.write('### READING GENE SET: [{}]\n'.format(args.gtf))
+    gtf = Anno(args.gtf, '')
+    gtf.addGtf()
+    gtf.norm_tx_format()
+    # dict keys: 'chr_strand_start_end' values: multiplicity
+    intron_keys = {}
+
+    for tx in gtf.transcripts.values():
+        introns = tx.get_type_coords(intron)
+        for i in introns.values():
+            for intron in i:
+                key = "{};{};{};{}".format(tx.chr, tx.strand, intron[0], intron[1])
+                if not key in intron_keys:
+                    intron_keys.update({key : 0})
+                intron_keys[key] += 1
+    output = []
+    for intron in intron_keys:
+        key = intron.split(';')
+        output.append([key[0], 'tsebra_hints', 'intron', int(key[2]), int(key[3]), \
+            intron_keys[intron], key[1], '.', 'mult={};pri=4;src={}'.\
+            format(intron_keys[intron], src)])
+    output.sort(key = lambda i:(i[3], i[4]))
+
+    with open(args.out, 'w+') as file:
+        out_writer = csv.writer(file, delimiter='\t', quotechar = "|", lineterminator = '\n')
+        for line in output:
+            out_writer.writerow(line)
+            
+def parseCmd():
+    """Parse command line arguments
+
+    Returns:
+        dictionary: Dictionary with arguments
+    """
+    parser = argparse.ArgumentParser(description='Detects all introns in the '\
+        + 'input gtf file and reports them as intron hints with a "src=" and a "mult=" attribute.')
+    parser.add_argument('-g', '--gtf', type=str, required=True,
+        help='Input gtf file with a set of transcripts.')
+    parser.add_argument('-s', '--src', type=str, required=False,
+        help='Source (single character) of the intron hints, default is "M" form manual hints.')
+    parser.add_argument('-o', '--out', type=str, required=True,
+        help='Outputfile for the intron hints in gtf.')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    main()
